887ba1c8af1b2ddeb81c28d33bbcf9353e011327
Tinty <158557314+Tinty86@users.noreply.github.com>
2024-02-08 14:45:01 +0300

    Update queue.py

--- queue.py
+++ queue.py
@@ -4,47 +4,56 @@ class Queue:
 
     class Customer:
         data = None
-        next_costumer = None
+        next_customer = None
 
-        def __init__(self, data, next_costumer = None):
+        def __init__(self, data, next_customer = None):
             self.data = data
-            self.next_costumer = next_costumer
+            self.next_customer = next_customer
 
     def append(self,data):
         if not self.head:
             self.head = self.Customer(data)
             return
-        costumer = self.head
-        while costumer.next_costumer:
-            costumer = costumer.next_costumer 
+        customer = self.head
+        while customer.next_customer:
+            customer = customer.next_customer
 
-        costumer.next_costumer = self.Customer(data)
+        customer.next_customer = self.Customer(data)
 
-    def first_out(self):
+    def first_out(self, ):
         if not self.head:
             return "Error: Empty queue"
-        self.head = self.head.next_costumer
+        head = self.head
+        self.head = head.next_customer
 
     def out(self):
-        costumer = self.head
+        customer = self.head
         if not self.head:
             return "Error: Empty queue"
-        if not self.head.next_costumer:
-            print(f"[{costumer.data}]")
+        if not self.head.next_customer:
+            print(f"[{customer.data}]")
             return 
         
-        print(f"[{costumer.data}] <- [{costumer.next_costumer.data}]", end="")
-        costumer = costumer.next_costumer
-        while costumer.next_costumer:
-            print(f" <- [{costumer.next_costumer.data}]")
-            costumer = costumer.next_costumer
+        customer = self.head
+        print(f"[{customer.data}]", end=" ")
+        
+        while customer.next_customer:
+            print(f"-> [{customer.next_customer.data}]", end=" ")
+            customer = customer.next_customer
+
+        
             
 
 queue = Queue()
 
 queue.append(1)
 queue.append(2)
+queue.append(3)
+queue.append(4)
+queue.append(5)
+queue.append(6)
 
 queue.first_out()
 
 queue.out()
+
